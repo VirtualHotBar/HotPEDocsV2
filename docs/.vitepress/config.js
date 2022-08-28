145f0c8caa51f950ba75dc01429e059a81e4680c
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2022-08-28 21:16:46 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,8 +7,18 @@ export default defineConfig({
   description: "一个纯净、强大、优雅的Win11PE",
   lastUpdated: true,
   base: "/",
+  head: [
+    // 改变title的图标
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: 'https://www.hotpe.top/wp-content/uploads/2022/04/logo.ico',
+      },
+    ],
+  ],
   themeConfig: {
-    logo: "https://www.hotpe.top/Other/HotPE_logo.png",
+    logo: "https://www.hotpe.top/wp-content/uploads/2022/04/logo.ico",
     sidebar,
     nav: [
       {
