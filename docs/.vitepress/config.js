500f87cfc0212de5fbf7debdbbfdd214bfe7c585
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2022-08-28 20:59:57 +0800

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from "vitepress";
+import sidebar from "./sidebar";
+
+export default defineConfig({
+  lang: "zh-CN",
+  title: "HotPE Docs",
+  description: "一个纯净、强大、优雅的Win11PE",
+  lastUpdated: true,
+  base: "/",
+  themeConfig: {
+    logo: "./favicon.ico",
+    sidebar,
+    nav: [
+      {
+        text: "首页",
+        link: "https://www.hotpe.top/",
+      },
+      {
+        text: "下载站",
+        link: "https://down.hotpe.top/",
+      },
+      {
+        text: "GitHub",
+        link: "https://www.hotpe.top/CodeWarehouse",
+      },
+    ],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2019-present VirtualHotBar",
+    },
+    editLink: {
+      style: 'github',
+      domain: 'github.com',
+      repo: 'VirtualHotBar/HotPEToolBox',
+      branch: 'main',
+      dir: 'docs',
+      text: '在 GitHub 上编辑此页',
+      pattern: 'https://github.com/VirtualHotBar/HotPEDocsV2/edit/main/docs/:path'
+    },
+    lastUpdatedText: "最近更新于",
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/VirtualHotBar/HotPEToolBox' }
+    ],
+  },
+});
