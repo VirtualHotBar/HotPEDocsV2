e8bdbe5f28827ef787448e30cd577e85e877e162
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-02-19 11:55:47 +0800

    Update sidebar.js

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -37,37 +37,32 @@ export default [
       { text: "使用Edgeless插件", link: "/course/edgeless" },
       { text: "将驱动打包成HotPE模块", link: "/course/driver_hpm" },
     ]
-  },  {
+  }, {
     text: "模块生态",
     items: [
-            { text: "制作HotPE模块", link: "/devdoc/makehpm" },
-          { text: "模块投稿", link: "/devdoc/hpm_con" },
+      { text: "制作HotPE模块", link: "/devdoc/makehpm" },
+      { text: "模块投稿", link: "/devdoc/hpm_con" },
 
       {
-    text: "开发者文档",
-    items: [
-
-      { text: "开放授权", link: "/cooperation/permit" },
-      { text: "项目规范", link: "/cooperation/standard" },
-      { text: "PE集成HotPE模块", link: "/devdoc/shifthpm" },
-      { text: "示例项目", 
-       items:[
-        { text: "GlassPE", link: "/cooperation/GlassPE" },
-      { text: "DawnPE", link: "/cooperation/DawnPE" },
-      { text: "CowPE", link: "/cooperation/CowPE" },
-      { text: "ComPE", link: "/cooperation/ComPE" },
-      { text: "PanDaPE", link: "/cooperation/PanDaPE" },
-      { text: "GlowPE", link: "/cooperation/GlowPE" },
-      ]},
-
-      
-      { text: "须知", link: "/devdoc/notice" },
-      { text: "环境", link: "/devdoc/dve_env" },
-      { text: "API", link: "/devdoc/API" },
-
-      { text: "修改HotPE成为分支", link: "/devdoc/branch" },
-    ]
-  },
+        text: "开发者文档",
+        items: [
+          { text: "开放授权", link: "/cooperation/permit" },
+          { text: "项目规范", link: "/cooperation/standard" },
+          { text: "PE集成HotPE模块", link: "/devdoc/shifthpm" },
+          {
+            text: "示例项目",
+            items: [
+              { text: "GlassPE", link: "/cooperation/GlassPE" },
+              { text: "DawnPE", link: "/cooperation/DawnPE" },
+              { text: "CowPE", link: "/cooperation/CowPE" },
+              { text: "ComPE", link: "/cooperation/ComPE" },
+              { text: "PanDaPE", link: "/cooperation/PanDaPE" },
+              { text: "GlowPE", link: "/cooperation/GlowPE" },
+            ]
+          },
+          { text: "修改HotPE成为分支", link: "/devdoc/branch" },
+        ]
+      },
 
     ]
   }
