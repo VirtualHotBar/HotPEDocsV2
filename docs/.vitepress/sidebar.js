994e921a2c9d1c521cdbc3586f775593fcebf250
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-02-19 11:52:34 +0800

    Update sidebar.js

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -37,21 +37,16 @@ export default [
       { text: "使用Edgeless插件", link: "/course/edgeless" },
       { text: "将驱动打包成HotPE模块", link: "/course/driver_hpm" },
     ]
-  }, {
-    text: "开发者文档",
-    items: [
-      { text: "须知", link: "/devdoc/notice" },
-      { text: "环境", link: "/devdoc/dve_env" },
-      { text: "API", link: "/devdoc/API" },
-
-      { text: "修改HotPE成为分支", link: "/devdoc/branch" },
-    ]
-  }, {
+  },  {
     text: "模块生态",
     items: [
             { text: "制作HotPE模块", link: "/devdoc/makehpm" },
           { text: "模块投稿", link: "/devdoc/hpm_con" },
-      
+
+      {
+    text: "开发者文档",
+    items: [
+
       { text: "开放授权", link: "/cooperation/permit" },
       { text: "项目规范", link: "/cooperation/standard" },
       { text: "PE集成HotPE模块", link: "/devdoc/shifthpm" },
@@ -64,6 +59,16 @@ export default [
       { text: "PanDaPE", link: "/cooperation/PanDaPE" },
       { text: "GlowPE", link: "/cooperation/GlowPE" },
       ]},
+
+      
+      { text: "须知", link: "/devdoc/notice" },
+      { text: "环境", link: "/devdoc/dve_env" },
+      { text: "API", link: "/devdoc/API" },
+
+      { text: "修改HotPE成为分支", link: "/devdoc/branch" },
+    ]
+  },
+
     ]
   }
 ];
